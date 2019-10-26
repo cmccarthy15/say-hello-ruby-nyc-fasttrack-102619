552a4d5f1d81154777d6dc5f4584698420dbeecf
Caryn McCarthy <caryn.mccarthy@flatironschool.com>
2019-10-26 18:26:45 +0000

    Automatically backed up by Learn

--- say_hello.rb
+++ say_hello.rb
@@ -1,7 +1,23 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 # Build your say_hello method here
-def say_hello(name)
+def say_hello(name="Gabriella")
   
   puts "Hello #{}!"
 end
 
-say_hello('Gabriella')
+# say_hello('Gabriella')
